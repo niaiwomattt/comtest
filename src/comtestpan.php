6b0e700e6433472a02b81121077af5a546f5a4ea
panbing <gongyong1990@126.com>
2019-05-22 14:43:11 +0800

    add comtestpan

---
+++ src/comtestpan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace comtestpan;
+
+class comtestpan
+{
+    static public function run ()
+    {
+        echo 'panbing';
+    }
+}
---
+++ tests/comtestpanTest.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * 数字格式化测试类
+ * @author XinLiang
+ */
+require '../vendor/autoload.php';
+use \comtestpan\comtestpan;
+
+comtestpan::run();
